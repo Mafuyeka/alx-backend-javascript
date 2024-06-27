4c511365ac9c349ba804c8514c2246b259b0a24b
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-06-27 11:27:41 -0700

    Updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-'''Module to find Pascal's Triangle integers'''
+'''A module for working with Pascal's triangle.
+'''
+
 
 def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
     '''
-    Function to find Pascal's Triangle integers
-
-        Parameters:
-            n (int): The number of rows of Pascal's triangle
-
-        Returns:
-            triangle (list): List of lists representing Pascal's triangle
-    '''
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for x in range(1, n):
-        row = [1]
-        for y in range(1, x):
-            row.append(triangle[x-1][y-1] + triangle[x-1][y])
-        row.append(1)
-        triangle.append(row)
-
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
     return triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
